28f6342e9217234f3712c0705edb986e6b6b18e0
Nakshi Shah <nakshishah3108@gmail.com>
2024-07-26 22:07:32 +0530

    clg info added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class clg_info(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    email = models.EmailField()
+    phone = models.IntegerField()
+    dept = models.CharField(max_length=100)
+    timestamp=models.DateField(blank=True)
+    
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from .models import clg_info
 
 # Create your views here.
 def home(request):
     return render(request,"navbar.html")
+
+def clg_info(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        address=request.POST.get('address')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        dept=request.POST.get('dept')
+        
+        date=datetime.today()
+        
+        
+        obj=clg_info(name=name,address=address,email=email,phone=phone,dept=dept,timestamp=date)
+        obj.save()
+        
+    return render(request , "navbar.html")
+        
+        
