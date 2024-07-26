1d7e71b58b9ef70162e03ff185e2b7c89bb0d0db
Nakshi Shah <nakshishah3108@gmail.com>
2024-07-27 03:34:34 +0530

    updated

--- home/views.py
+++ home/views.py
@@ -29,19 +29,44 @@ def quiz (request):
     try:
         email = request.session['email']
         level =  Users_main.objects.get(email=email).level
-        questions = Quiz.objects.filter(level=level)
-        question1=[]
-        final=[]
-        
-        for question in range(5):
+        tech =  Users_main.objects.get(email=email).tech
+        questions = Quiz.objects.filter(level=level , tech=tech)
+        final = random.sample(questions, 5) 
             
-            nq=random.choices(questions)
-            if nq not in question1:
-                final.append(nq)
-                
         print(final)
+    
                 
-
+                
     except Exception as e:
         print(e)
-    return HttpResponse("hrllo")
+    return HttpResponse("hello")
+
+def quiz_ans_beg(request):
+    
+    email = request.session['email']
+    level =  Users_main.objects.get(email=email).level
+    if level == "begineer":
+        if request.method=="POST":
+                
+            ans1 = request.POST.get('ans1')
+            q1= request.POST.get('q1')
+
+            ans2 = request.POST.get('ans2')
+            q2 = request.POST.get( 'q2')
+
+            ans3 = request.POST.get('ans3')
+            q3 = request.POST.get( 'q3')
+            
+            ans4 = request.POST.get('ans4')
+            q4 = request.POST.get( 'q4')
+            
+            ans5 = request.POST.get('ans5')
+            q5 = request.POST.get( 'q5')
+            
+            ans1 == Quiz.objects.get(question=q1).ans
+            ans2 == Quiz.objects.get(question = q2).ans
+            ans3 == Quiz.objects.get(question = q3).ans
+            ans4 == Quiz.objects.get(question = q4).ans
+            ans5 == Quiz.objects.get(question = q5).ans
+
+
