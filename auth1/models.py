861fc87b3f78be0f4f0a7002ae0f3a98eea5786d
Nakshi Shah <nakshishah3108@gmail.com>
2024-07-26 18:38:59 +0530

    updated models

--- auth1/models.py
+++ auth1/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password,check_password
+from django.utils import timezone
 
 # Create your models here.
 class Visit(models.Model):
@@ -7,3 +9,44 @@ class Visit(models.Model):
 
     def __str__(self):
         return f"{self.page_visited} - {self.visit_count}"
+
+class Users (models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    city = models.CharField(max_length=50)
+    qualication = models.CharField(max_length=100)
+    profile_pic = models.TextField()
+    dob = models.DateField()
+    bio = models.TextField()
+    desciption = models.TextField()
+    time_stamp = models.DateField()
+    is_active = models.BooleanField(default=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+    
+    
+class otps(models.Model):
+    email = models.EmailField(max_length=100)
+    otp = models.IntegerField()
+    created_at = models.DateField()
+    expires_at = models.DateField()
+    
+    def is_expired(self):
+        return timezone.now() > self.expires_at
+
+    def __str__(self):
+        return self.email
+    
+    
