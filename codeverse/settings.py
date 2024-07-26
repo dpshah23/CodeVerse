a845dc35c7fbe06af465b6336d707fd6a1aac56e
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-07-26 17:41:37 +0530

    updated urls and added static file urls

--- codeverse/settings.py
+++ codeverse/settings.py
@@ -59,7 +59,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
