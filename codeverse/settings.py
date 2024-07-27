ff7e208146252e168c002567ffb134ff282fd34e
Nakshi Shah <nakshishah3108@gmail.com>
2024-07-27 09:10:59 +0530

    community chat feature done

--- codeverse/settings.py
+++ codeverse/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'auth1',
     'home',
     'error_handling',
@@ -77,7 +78,22 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'codeverse.wsgi.application'
+
+ASGI_APPLICATION = 'codeverse.asgi.application'
+
+CHANNEL_LAYERS={
+    'default':{
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    }
+}
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
 
 
 # Database
